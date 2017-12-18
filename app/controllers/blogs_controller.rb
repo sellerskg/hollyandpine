c8c9c258e6fee4c07bded02d7803950c90e26237
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-12-17 21:20:14 -0500

    Merge branch 'master' of https://github.com/sellerskg/hollyandpine
    # Conflicts:
    #	app/controllers/blogs_controller.rb

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,7 @@
 class BlogsController < ApplicationController
   def index
     @blogs = Blog.active.order(:created_at).reverse
+    @blogs = Blog.where(active: true).paginate(page: params[:page], per_page: 9).order('created_at DESC')
   end
 
   def show
